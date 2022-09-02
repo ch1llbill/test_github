0475251b795691d879260c563b067eec87c07192
Bill Jiang <billjiang2002@gmail.com>
2022-09-02 13:27:36 -0400

    added square function

--- math.cpp
+++ math.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+int square(int n){
+  return n*n;
+}
+
 int main(){
   std::cout << "Printing math" << std::endl;
+  int n = 8;
+  std::cout << square(n) << square(5) << std::endl;
 }
